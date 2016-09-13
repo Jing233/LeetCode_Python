4ffab2ab43447d8c772f7d3ed76aa486102be126
Jing Xiao <jasonfreedom@sina.com>
2016-09-13 16:10:05 -0400

    [215. Kth Largest Element in an Array][Accepted]committed by Jing

--- 215-Kth-Largest-Element-in-an-Array/solution.py
+++ 215-Kth-Largest-Element-in-an-Array/solution.py
@@ -5,12 +5,26 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        import heapq
+        # use partitioning in quickSort
         
-        minHeap = []
-        for num in nums:
-            if len(minHeap) < k:
-                heapq.heappush(minHeap, num)
-            elif minHeap[0] < num:
-                heapq.heappushpop(minHeap, num)
-        return minHeap[0]
+        def partition(nums, start, end):
+            pivot = nums[end]
+            left = start
+            for i in range(start, end):
+                if nums[i] <= pivot:
+                    nums[i], nums[left] = nums[left], nums[i]
+                    left += 1
+            nums[left], nums[end] = nums[end], nums[left]
+            return left
+            
+            
+        pos = partition(nums, 0, len(nums) - 1)
+        print nums
+        while pos != len(nums) - k:
+            if pos > len(nums) - k:
+                pos = partition(nums, 0, pos - 1)
+            else:
+                pos = partition(nums, pos + 1, len(nums) - 1)
+
+        return nums[pos]
+        
