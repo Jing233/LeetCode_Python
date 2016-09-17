b6c6c0891767c7c4745f60be60938e3a249b4150
Jing Xiao <jasonfreedom@sina.com>
2016-09-17 16:01:13 -0400

    [1. Two Sum][Accepted]committed by Jing

--- 1-Two-Sum/solution.py
+++ 1-Two-Sum/solution.py
@@ -5,12 +5,16 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        hashTable = {}
-        for i in range(len(nums)):
-            temp = target - nums[i]
-            if temp in hashTable:
-                return [hashTable[temp], i] 
-            hashTable[nums[i]] = i
+        indexOrder = sorted(range(len(nums)), key=lambda i: nums[i])
         
-            
+        left = 0
+        right = len(nums) - 1
+        while left < right:
+            temp = nums[indexOrder[left]] + nums[indexOrder[right]]
+            if temp > target:
+                right -= 1
+            elif temp < target:
+                left += 1
+            else:
+                return [indexOrder[left], indexOrder[right]]
         
