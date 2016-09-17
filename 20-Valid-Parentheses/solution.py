0b83154b9bf8567fc2f46b313ed4870425298745
Jing Xiao <jasonfreedom@sina.com>
2016-09-17 17:22:18 -0400

    [20. Valid Parentheses][Accepted]committed by Jing

--- 20-Valid-Parentheses/solution.py
+++ 20-Valid-Parentheses/solution.py
@@ -4,17 +4,19 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+        # keypoint 1: ']'
+        # keypoint 2: '['
+        
         mapping = {'(':')', '[':']', '{':'}'}
         leftStack = []
-        
         for ch in s:
             if ch in '([{':
                 leftStack.append(ch)
             else:
-                if leftStack == [] or mapping[leftStack.pop()] != ch:
+                if leftStack == [] or mapping[leftStack.pop()] != ch:    
                     return False
         
-        if leftStack == []:
+        if leftStack == []:                                             
             return True
         else:
             return False
