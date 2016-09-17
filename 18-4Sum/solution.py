5a1ca1c47dee171c5157c0e609019b68242843e2
Jing Xiao <jasonfreedom@sina.com>
2016-09-17 17:05:54 -0400

    [18. 4Sum][Accepted]committed by Jing

---
+++ 18-4Sum/solution.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        
+        nums = sorted(nums)
+        res = []
+        for i in range(len(nums)):
+            if i and nums[i] == nums[i - 1]:
+                continue
+            for j in range(i + 1, len(nums)):
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+                left = j + 1
+                right = len(nums) - 1
+                while left < right:
+                    fourSum = nums[i] + nums[j] + nums[left] + nums[right]
+                    
+                    if fourSum > target:
+                        right -= 1
+                    elif fourSum < target:
+                        left += 1
+                    else:
+                        res.append([nums[i], nums[j], nums[left], nums[right]])
+                        left += 1
+                        right -= 1
+                        while left < right and nums[left] == nums[left - 1]:
+                            left += 1
+                        while right > left and nums[right] == nums[right + 1]:
+                            right -= 1
+                            
+        return res
+                
