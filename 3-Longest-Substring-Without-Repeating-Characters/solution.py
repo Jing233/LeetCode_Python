d06b7646fea823d6cac70b2ebdcf04dd4958b920
Jing Xiao <jasonfreedom@sina.com>
2016-09-18 22:49:53 -0400

    [3. Longest Substring Without Repeating Characters][Accepted]committed by Jing

---
+++ 3-Longest-Substring-Without-Repeating-Characters/solution.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        
+        hashTable = {}
+        right = 0
+        left = 0
+        maxLen = 0
+        while right < len(s):
+            #print hashTable
+            if s[right] in hashTable:
+                while left < right and s[right] in hashTable:
+                    del hashTable[s[left]]
+                    left += 1
+            hashTable[s[right]] = True
+            right += 1
+            maxLen = max(maxLen, right - left)
+        return maxLen
