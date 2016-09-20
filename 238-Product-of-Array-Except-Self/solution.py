c3b897f83f49f650a4f71e2bff77f6958e1b8580
Jing Xiao <jasonfreedom@sina.com>
2016-09-19 23:55:59 -0400

    [238. Product of Array Except Self][Accepted]committed by Jing

---
+++ 238-Product-of-Array-Except-Self/solution.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        prodFromHead = [1 for i in nums]
+        prodFromTail = [1 for i in nums]
+        for i, _ in enumerate(nums):
+            if i >= 1:
+                prodFromHead[i] = prodFromHead[i - 1] * nums[i - 1]
+        
+        res = [1 for i in nums]
+        res[-1] = prodFromHead[-1]
+        for i in reversed(xrange(len(nums) - 1)):
+            prodFromTail[i] = prodFromTail[i + 1] * nums[i + 1]
+            res[i] = prodFromTail[i] * prodFromHead[i]
+        
+        return res
+        
