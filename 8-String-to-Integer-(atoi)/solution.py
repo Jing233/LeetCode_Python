9773747304b13420b38277d54fc944b0d92722ab
Jing Xiao <jasonfreedom@sina.com>
2016-09-20 15:34:48 -0400

    [8. String to Integer (atoi)][Accepted]committed by Jing

--- 8-String-to-Integer-(atoi)/solution.py
+++ 8-String-to-Integer-(atoi)/solution.py
@@ -7,26 +7,24 @@ class Solution(object):
         str = str.strip()
         if not str:
             return 0
-        sign = 1
         start = 0
+        sign = 1
         if str[0] == '+':
-            start = 1
+            start += 1
         elif str[0] == '-':
-            start = 1
+            start += 1
             sign = -1
-            
         val = 0
-        for ch in str[start:]:
-            if ch in '0123456789':
-                val = 10 * val + ord(ch) - ord('0')
+        for i in range(start, len(str)):
+            if str[i] in '0123456789':
+                val = 10 * val + ord(str[i]) - ord('0')
             else:
                 break
+        
+        val *= sign
+        if val > 2**31 - 1:
+            return 2**31 - 1
+        elif val <= -2**31:
+            return -2**31
+        return val 
             
-        val = sign * val
-        if val > 2 ** 31 - 1:
-            return 2 ** 31 - 1
-        elif val < -2 ** 31:
-            return -2 ** 31
-        else:
-            return val
-         
