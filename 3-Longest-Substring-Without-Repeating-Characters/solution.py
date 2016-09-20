abd0103a8a4757d846ee58e29347e19790a0f455
Jing Xiao <jasonfreedom@sina.com>
2016-09-19 21:12:06 -0400

    [3. Longest Substring Without Repeating Characters][Accepted]committed by Jing

--- 3-Longest-Substring-Without-Repeating-Characters/solution.py
+++ 3-Longest-Substring-Without-Repeating-Characters/solution.py
@@ -4,17 +4,20 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        
         hashTable = {}
-        right = 0
         left = 0
         maxLen = 0
-        while right < len(s):
-            if s[right] in hashTable:
-                while s[right] in hashTable:
-                    del hashTable[s[left]]
-                    left += 1
-            hashTable[s[right]] = True
-            right += 1
-            maxLen = max(maxLen, right - left)
-        return maxLen
+        resLeft = 0
+        resRight = 0
+        for right in range(len(s)):
+            hashTable[s[right]] = hashTable.get(s[right], 0) + 1
+            while hashTable[s[right]] > 1:
+                hashTable[s[left]] -= 1
+                left += 1
+            if right - left > maxLen:
+                maxLen = right - left
+                resLeft = left
+                resRight = right
+                
+        return len(s[resLeft : resRight + 1])
+        
