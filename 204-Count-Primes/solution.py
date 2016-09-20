74ef226874c13f1ba6ed5a21f279d6b09c6bf20c
Jing Xiao <jasonfreedom@sina.com>
2016-09-20 11:50:05 -0400

    [204. Count Primes][Accepted]committed by Jing

---
+++ 204-Count-Primes/solution.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def countPrimes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        import math
+        
+        if n <= 1:
+            return 0
+            
+        primeTable = [True for i in xrange(n)]
+        primeTable[0], primeTable[1] = False, False
+        
+        for i in range(2, int(math.sqrt(n)) + 1):
+            if primeTable[i]:
+                multi = 2
+                while multi * i < n:
+                    primeTable[multi * i] = False
+                    multi += 1
+        counter = 0
+        for prime in primeTable:
+            if prime:
+                counter += 1
+        return counter
