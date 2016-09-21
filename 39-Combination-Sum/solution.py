38ae06e11d1f159564f3333a73730f1f99479fd0
Jing Xiao <jasonfreedom@sina.com>
2016-09-20 22:42:27 -0400

    [39. Combination Sum][Accepted]committed by Jing

--- 39-Combination-Sum/solution.py
+++ 39-Combination-Sum/solution.py
@@ -5,15 +5,15 @@ class Solution(object):
         :type target: int
         :rtype: List[List[int]]
         """
-        def dfs(candidates, path, tempSum, target, res):
+        def dfs(candidates, start, path, tempSum, target, res):
             if tempSum > target:
                 return
             elif tempSum == target:
                 res.append(path)
                 return
-            for i in range(len(candidates)):
-                dfs(candidates[i:], path + [candidates[i]], tempSum + candidates[i], target, res)
+            for i in range(start, len(candidates)):
+                dfs(candidates, i, path + [candidates[i]], tempSum + candidates[i], target, res)
         
         res = []
-        dfs(candidates, [], 0, target, res)
+        dfs(candidates, 0, [], 0, target, res)
         return res
