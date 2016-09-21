bec18fa62535deb6268217477044fbbeefa41182
Jing Xiao <jasonfreedom@sina.com>
2016-09-20 23:23:07 -0400

    [127. Word Ladder][Accepted]committed by Jing

--- 127-Word-Ladder/solution.py
+++ 127-Word-Ladder/solution.py
@@ -6,23 +6,25 @@ class Solution(object):
         :type wordList: Set[str]
         :rtype: int
         """
-        length = 2
-        front, back = set([beginWord]), set([endWord])
-        wordDict.add(beginWord)
-        wordDict.add(endWord)
+        front, back=set([beginWord]), set([endWord]) 
+        length=2
+        width=len(beginWord)
+        charSet=list(string.lowercase)
+        wordDict.discard(beginWord)
+        wordDict.discard(endWord)
         while front:
-            # generate all valid transformations
-            front = wordDict & (set(word[:index] + ch + word[index+1:] 
-                                for word in front 
-                                    for index in range(len(beginWord))
-                                        for ch in 'abcdefghijklmnopqrstuvwxyz'))
-            if front & back:
-                # there are common elements in front and back, done
-                return length
-            length += 1
-            if len(front) > len(back):
-                # swap front and back for better performance (fewer choices in generating nextSet)
-                front, back = back, front
-            # remove transformations from wordDict to avoid cycle
-            wordDict -= front
+            newFront=set()
+            for phrase in front:
+                for i in xrange(width):
+                    for c in charSet:
+                        nw=phrase[:i]+c+phrase[i+1:]
+                        if nw in back:
+                            return length
+                        if nw in wordDict:
+                            newFront.add(nw)
+            front=newFront
+            if len(front)>len(back):
+                front,back=back,front
+            wordDict-=front
+            length+=1
         return 0
