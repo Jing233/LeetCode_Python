dc411009474ba61d7d551c1c73279cd2fa9cb864
Jing Xiao <jasonfreedom@sina.com>
2016-09-22 11:07:09 -0400

    [283. Move Zeroes][Accepted]committed by Jing

---
+++ 283-Move-Zeroes/solution.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                continue
+            else:
+                j = i
+                while j < len(nums) - 1 and nums[j] == 0:
+                    j += 1
+                nums[i], nums[j] = nums[j], nums[i]
+                if j == len(nums) - 1:
+                    break
+        
+        
+        
