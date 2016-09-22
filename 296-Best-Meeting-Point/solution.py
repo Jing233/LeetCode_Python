93e68489567c4c9ad033043c5ea86154d81b2c2d
Jing Xiao <jasonfreedom@sina.com>
2016-09-21 22:09:54 -0400

    [296. Best Meeting Point][Accepted]committed by Jing

---
+++ 296-Best-Meeting-Point/solution.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def minTotalDistance(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        
+        xPositions = []
+        yPositions = []
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    xPositions.append(i)
+                    yPositions.append(j)
+        
+        xPositions = sorted(xPositions)
+        yPositions = sorted(yPositions)
+        minDistance = 0
+        left = 0
+        right = len(xPositions) - 1
+        while left < right:
+            minDistance += xPositions[right] - xPositions[left] + yPositions[right] - yPositions[left]
+            left += 1
+            right -= 1
+        return minDistance
