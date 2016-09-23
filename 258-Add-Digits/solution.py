5d6d6dc1f2cf34587f0a16e0a51f2cff33dea467
Jing Xiao <jasonfreedom@sina.com>
2016-09-23 16:21:37 -0400

    [258. Add Digits][Accepted]committed by Jing

--- 258-Add-Digits/solution.py
+++ 258-Add-Digits/solution.py
@@ -4,6 +4,9 @@ class Solution(object):
         :type num: int
         :rtype: int
         """
-        while num >= 10:
-            num = num / 10 + num % 10
-        return num
+        if num == 0:
+            return num
+        elif num % 9 == 0:
+            return 9
+        else:
+            return num % 9
