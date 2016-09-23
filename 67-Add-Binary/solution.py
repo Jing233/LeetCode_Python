896735ee3b60b8e609cbbdc363cdca2ec8ef1b80
Jing Xiao <jasonfreedom@sina.com>
2016-09-23 16:05:36 -0400

    [67. Add Binary][Accepted]committed by Jing

--- 67-Add-Binary/solution.py
+++ 67-Add-Binary/solution.py
@@ -8,16 +8,16 @@ class Solution(object):
         i = len(a) - 1
         j = len(b) - 1
         carry = 0
-        res = ''
+        res = []
         while i >=0 or j >= 0:
             val1 = int(a[i]) if i >= 0 else 0
             val2 = int(b[j]) if j >= 0 else 0
             val = (val1 + val2 + carry) % 2
             carry = (val1 + val2 + carry) / 2
-            res += str(val)
+            res.append(str(val))
             i -= 1
             j -= 1
         
         if carry:
-            res += '1'
-        return res[::-1]
+            res.append('1')
+        return ''.join(reversed(res))
