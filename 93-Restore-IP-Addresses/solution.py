3fd8ec7030f4feded4cc3fa1856b903390bfc708
Jing Xiao <jasonfreedom@sina.com>
2016-09-23 21:37:00 -0400

    [93. Restore IP Addresses][Accepted]committed by Jing

--- 93-Restore-IP-Addresses/solution.py
+++ 93-Restore-IP-Addresses/solution.py
@@ -5,14 +5,12 @@ class Solution(object):
         :rtype: List[str]
         """
         def isValidPart(part):
-            if len(part) == 1:
-                return True
+            if len(part) == 3:
+                return 100 <= int(part) <= 255
             elif len(part) == 2:
                 return part[0] != '0'
-            elif len(part) == 3:
-                return 100 <= int(part) <= 255
             else:
-                return False
+                return True
         
         def dfs(s, path, res):
             if s == '' and len(path) == 4:
@@ -23,9 +21,10 @@ class Solution(object):
             for i in range(1, min(4, len(s)+1)):
                 if isValidPart(s[:i]):
                     dfs(s[i:], path + [s[:i]], res)
-        if len(s) > 12:
-            return []
+        
         res = []
+        if len(s) > 12 or len(s) < 4:
+            return res
         dfs(s, [], res)
         return res
                 
