664a2624c92a472a5d67bb0d48ecc7eb4cac13cd
Jing Xiao <jasonfreedom@sina.com>
2016-09-24 00:02:00 -0400

    [257. Binary Tree Paths][Accepted]committed by Jing

--- 257-Binary-Tree-Paths/solution.py
+++ 257-Binary-Tree-Paths/solution.py
@@ -9,16 +9,4 @@ class Solution:
     # @param {TreeNode} root
     # @return {string[]}
     def binaryTreePaths(self, root):
-        def recur(root, path, res):
-            if root.left is None and root.right is None:
-                res.append(path)
-                return
-            if root.left:
-                recur(root.left, path + '->' + str(root.left.val), res)
-            if root.right:
-                recur(root.right, path + '->' + str(root.right.val), res)
-    
-        res = []
-        if root:
-            recur(root, str(root.val), res)
-        return res
+        
