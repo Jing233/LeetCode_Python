58a74962dcda0023619fdeedd68905243b41c8a1
Jing Xiao <jasonfreedom@sina.com>
2016-10-03 14:31:58 -0400

    [127. Word Ladder][Accepted]committed by Jing

--- 127-Word-Ladder/solution.py
+++ 127-Word-Ladder/solution.py
@@ -7,23 +7,21 @@ class Solution(object):
         :rtype: int
         """
         import string
-        def bfs(wordList, toVisit):
-            depth = 1
-            while toVisit:
-                tempToVisit = []
-                for word in toVisit:
-                    for i in range(len(word)):
-                        for c in string.lowercase:
-                            nextWord = word[:i] + c + word[i + 1:]
-                            if nextWord == endWord:
-                                return depth + 1
-                            if nextWord in wordList:
-                                wordList.remove(nextWord)
-                                tempToVisit.append(nextWord)
-                toVisit = tempToVisit
-                depth += 1
-            return 0
             
         toVisit = [beginWord]
         wordList -= {beginWord, endWord}
-        return bfs(wordList, toVisit)
+        depth = 1
+        while toVisit:
+            tempToVisit = []
+            for word in toVisit:
+                for i in range(len(word)):
+                    for c in string.lowercase:
+                        nextWord = word[:i] + c + word[i + 1:]
+                        if nextWord == endWord:
+                            return depth + 1
+                        if nextWord in wordList:
+                            wordList.remove(nextWord)
+                            tempToVisit.append(nextWord)
+            toVisit = tempToVisit
+            depth += 1
+        return 0
