4d3e8c208d19d5e7cdf660b98b25da52087a9a12
Jing Xiao <jasonfreedom@sina.com>
2016-10-03 10:14:14 -0400

    [329. Longest Increasing Path in a Matrix][Accepted]committed by Jing

--- 329-Longest-Increasing-Path-in-a-Matrix/solution.py
+++ 329-Longest-Increasing-Path-in-a-Matrix/solution.py
@@ -12,23 +12,18 @@ class Solution(object):
             for dir in [(1,0),(-1,0),(0,1),(0,-1)]:
                 newI = i + dir[0]
                 newJ = j + dir[1]
-                if isValid(newI, newJ) and matrix[newI][newJ] > matrix[i][j]:
+                if 0 <= newI < m and 0 <= newJ < n and matrix[newI][newJ] > matrix[i][j]:
                     tmp = max(tmp, DP(newI, newJ))
             tmp += 1
             cache[i][j] = tmp
             return tmp
-        
-        def isValid(i, j):
-            if 0 <= i < m and 0 <= j < n:
-                return True
-            return False
     
     
         if not matrix:
             return 0
         m = len(matrix)
         n = len(matrix[0])
-        cache = [[0] * n for i in range(m)]
+        cache = [[0] * n for i in xrange(m)]
         maxLen = 0
         for i in range(m):
             for j in range(n):
